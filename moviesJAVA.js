39f16a82680f2de384e4c9b5281136f4bf28530d
stevee14 <nathandonnet14@gmail.com>
2025-01-20 14:13:39 +0100

    movies et afficher les film
    bon code fait jeudi et permet d'avoir les info sur les film et de les afficher dans une page avec du css

---
+++ moviesJAVA.js
@@ -0,0 +1,34 @@
+const cleApi = "61317a5a";
+const paramsUrl = new URLSearchParams(window.location.search);
+const idFilm = paramsUrl.get('id');
+
+const recupererDetailsFilm = async () => {
+    const url = `https://www.omdbapi.com/?apikey=${cleApi}&i=${idFilm}`;
+    try {
+        const reponse = await fetch(url);
+        const film = await reponse.json();
+
+        if (film.Response === "True") {
+            afficherDetailsFilm(film);
+        } else {
+            document.getElementById('detailsFilm').innerHTML = "<p>Aucun détail trouvé pour ce film</p>";
+        }
+    } catch (erreur) {
+        console.error("Erreur lors de la récupération des détails du film:", erreur);
+    }
+};
+
+const afficherDetailsFilm = (film) => {
+    const divDetailsFilm = document.getElementById('detailsFilm');
+    divDetailsFilm.innerHTML = `
+        <h1>${film.Title}</h1>
+        <img src="${film.Poster}" alt="${film.Title}">
+        <p><strong>Année :</strong> ${film.Year}</p>
+        <p><strong>Genre :</strong> ${film.Genre}</p>
+        <p><strong>Réalisateur :</strong> ${film.Director}</p>
+        <p><strong>Acteurs :</strong> ${film.Actors}</p>
+        <p><strong>Intrigue :</strong> ${film.Plot}</p>
+    `;
+};
+
+recupererDetailsFilm();
